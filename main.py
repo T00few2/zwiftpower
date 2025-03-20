0022eef08e390ba7457219ab47dbb7be114dcaf0
T00few2 <christian.kjar@gmail.com>
2025-03-20 21:34:01 +0100

    flask

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # main.py
-def hello_world(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using `make_response
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json()
-    if request_json and 'name' in request_json:
-        name = request_json['name']
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        request_json = request.get_json()
+        if request_json and 'name' in request_json:
+            name = request_json['name']
+        else:
+            name = 'World'
+        return f'Hello {name}!'
     else:
-        name = 'World'
-    return f'Hello {name}!'
+        return 'Hello World!'
