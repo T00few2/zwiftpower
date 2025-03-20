58f5b55bb4cca93bd1c2059108174ff92610cc6b
T00few2 <christian.kjar@gmail.com>
2025-03-20 20:35:58 +0100

    ff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import functions_framework
+from flask import Response, request
 
 @functions_framework.http
-def app(request):
-    # Your logic here
+def hello_world(request):
+    # Check if the request is for /favicon.ico
+    if request.path == '/favicon.ico':
+        return Response(status=204)
     return "Hello, world!"
